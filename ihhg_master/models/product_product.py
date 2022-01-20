e6099b95e75dcbe2919a55b86a50643a6d881f50
hafiz21 <hafiz@portcities.net>
2022-01-20 12:35:58 +0100

    [FIX] remove product.product since its not in use.

--- ihhg_master/models/product_product.py
+++
@@ -1,8 +0,0 @@
-
-from odoo import models, fields, api
-
-
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
-
-    originating_project = fields.Many2one(string='Originating Project') # Do not understand the purpose of this
