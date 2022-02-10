2e4044d0108b218558405010e7bc5001465fd323
hafiz21 <hafiz@portcities.net>
2022-02-10 16:49:33 +0100

    [ADD} Filtering for Channel, Package and Item selection. Campaign type model

--- ihhg_master/models/scm_entry.py
+++ ihhg_master/models/scm_entry.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class SCMEntry(models.Model):
@@ -12,10 +12,10 @@ class SCMEntry(models.Model):
     user_id = fields.Many2one('res.users', string='Owner', default=lambda self: self.env.user, tracking=True)
     user_project_id = fields.Many2one('res.users', string='Project Manager', tracking=True)
     channel_id = fields.Many2many('ihh.channel', string='Channel', tracking=True)
-    package_id = fields.Many2many('ihh.package', string='Package', tracking=True)
-    item_id = fields.Many2many('ihh.item', string='Item', tracking=True)
+    package_id = fields.Many2many('ihh.package', string='Package', store=True, tracking=True, domain="[('channel_id', '=', channel_id)]")
+    item_id = fields.Many2many('ihh.item', string='Item', store=True, tracking=True, domain="[('package_id', '=', package_id)]")
     category_id = fields.Many2one('ihh.category', string='Campaign Category', tracking=True)
     originating_regulation_id = fields.Many2one('store.regulation.entry', string='Originating Regulation', tracking=True)
-    product_tmpl_id = fields.Many2one('product.template', string='Campaign Type', tracking=True)
+    campaign_type_id = fields.Many2one('campaign.type', string='Campaign Type', tracking=True)
     date_from = fields.Datetime(string='Campaign Start', tracking=True)
     date_to = fields.Datetime(string='Campaign Stop', tracking=True)
