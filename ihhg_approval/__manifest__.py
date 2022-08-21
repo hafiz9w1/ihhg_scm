82cc1f882bbd09935cc209cf503d10280fb2ed95
Hafiz Abbas <abbas.ext@arrival.com>
2022-08-21 18:12:22 +0100

    [IMP] fix pylint error

--- ihhg_approval/__manifest__.py
+++ ihhg_approval/__manifest__.py
@@ -1,11 +1,12 @@
 {
-    'name': "iHH Group Custom Addon - Approval",
-    'version': "15.0.1.0.1",
+    'name': 'iHH Group Custom Addon - Approval',
+    'version': '15.0.1.0.1',
     'category': 'management',
     'summary': """ Custom module""",
 
     'description': """
-    This module adds the following functionalities to extend the approval Module.
+    This module adds the following functionalities to
+    extend the approval Module.
 
 Functionalities:
 - Extend approval category for has_scm
@@ -14,9 +15,8 @@ Functionalities:
 
     """,
 
-    'author': "Hafiz Abbas",
-    'email': "hafiz@portcities.net",
-    'website': "http://portcities.net",
+    'author': 'Hafiz Abbas',
+    'email': 'hafizabbas9w1@gmail.com',
     'depends': [
         'ihhg_master',
         'approvals',
